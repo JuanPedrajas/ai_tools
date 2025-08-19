d207a562f079331d5c99d8c72b676da208c133d5
Juan Pedrajas Mendoza <juan.pedrajas@ebury.com>
2025-08-19 12:09:23 +0200

    feat: generated tool skeleton

---
+++ src/review_command.sh
@@ -0,0 +1,5 @@
+echo "# This file is located at 'src/review_command.sh'."
+echo "# It contains the implementation for the 'juanbot review' command."
+echo "# The code you write here will be wrapped by a function named 'juanbot_review_command()'."
+echo "# Feel free to edit this file; your changes will persist when regenerating."
+inspect_args
